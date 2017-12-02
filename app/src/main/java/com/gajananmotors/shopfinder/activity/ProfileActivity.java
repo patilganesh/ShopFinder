99bce9d8b7a4712a0e04a9d4351697078c565c9b
Gajanan Motars <patilganesh6200@gmail.com>
2017-12-02 11:06:15 +0530

    profile edit

--- app/src/main/java/com/gajananmotors/shopfinder/activity/ProfileActivity.java
+++ app/src/main/java/com/gajananmotors/shopfinder/activity/ProfileActivity.java
@@ -2,14 +2,37 @@ package com.gajananmotors.shopfinder.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.gajananmotors.shopfinder.R;
 
+
+
 public class ProfileActivity extends AppCompatActivity {
+
+    EditText etName,etEmail,etMobile,etDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        etName=findViewById(R.id.etName);
+        etEmail=findViewById(R.id.etEmail);
+        etMobile=findViewById(R.id.etMobile);
+        etDate=findViewById(R.id.etDate);
+        Button btnUpdate = findViewById(R.id.btnUpdate);
+        ImageButton imgProfile = findViewById(R.id.imgProfile);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
