3ab4912f9a48fcdfd424881bb784ec8aaaf9777d
Gajanan Motars <patilganesh6200@gmail.com>
2017-12-21 11:57:59 +0530

    showdetails

--- app/src/main/java/com/gajananmotors/shopfinder/activity/MainActivity.java
+++ app/src/main/java/com/gajananmotors/shopfinder/activity/MainActivity.java
@@ -131,6 +131,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
