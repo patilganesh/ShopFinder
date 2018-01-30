757aebae61a01b4b1f8db361ba24bf6eb1059119
vasundhara-2 <vasundharagawade@gmail.com>
2018-01-30 13:08:40 +0530

    Changes In ViewPostActivity

--- app/src/main/java/com/gajananmotors/shopfinder/activity/ViewPostActivity.java
+++ app/src/main/java/com/gajananmotors/shopfinder/activity/ViewPostActivity.java
@@ -1,20 +1,19 @@
 package com.gajananmotors.shopfinder.activity;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Gallery;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gajananmotors.shopfinder.R;
-import com.gajananmotors.shopfinder.adapter.CustomGalleryAdapter;
 
 import static com.gajananmotors.shopfinder.common.CheckSetting.displayPromptForEnablingData;
 import static com.gajananmotors.shopfinder.common.CheckSetting.isNetworkAvailable;
@@ -22,8 +21,10 @@ import static com.gajananmotors.shopfinder.common.GeoAddress.getAddress;
 
 public class ViewPostActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private LinearLayout shopDirectionLayout,shopShareLayout,shopEditLayout,shopGallaryLayout;
-    private LinearLayout gallaryLayout,viewPostLayout;
+    private LinearLayout viewPostLayout, shopDirectionLayout, shopShareLayout, shopEditLayout, shopGallaryLayout, shopCallLayout, shopMsgLayout;
+    private TextView mob;
+    private String mob_number;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +34,22 @@ public class ViewPostActivity extends AppCompatActivity implements View.OnClickL
         viewPostLayout = findViewById(R.id.viewPostLayout);
         shopDirectionLayout = findViewById(R.id.shopDirectionLayout);
         shopDirectionLayout.setOnClickListener(this);
-        shopGallaryLayout=findViewById(R.id.shopGallaryLayout);
+        shopGallaryLayout = findViewById(R.id.shopGallaryLayout);
         shopGallaryLayout.setOnClickListener(this);
-        shopEditLayout=findViewById(R.id.shopEditLayout);
+        shopEditLayout = findViewById(R.id.shopEditLayout);
         shopEditLayout.setOnClickListener(this);
+        shopCallLayout = findViewById(R.id.shopCallLayout);
+        shopCallLayout.setOnClickListener(this);
+        shopMsgLayout = findViewById(R.id.shopMsgLayout);
+        shopMsgLayout.setOnClickListener(this);
+        mob = findViewById(R.id.tvMobile);
+        mob_number = mob.getText().toString();
     }
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.shopDirectionLayout:
                 if (!isNetworkAvailable(this)) {
                     displayPromptForEnablingData(this);
@@ -60,15 +67,33 @@ public class ViewPostActivity extends AppCompatActivity implements View.OnClickL
             case R.id.shopGallaryLayout:
                 startActivity(new Intent(this, GallaryActivity.class));
                 return;
+            case R.id.shopCallLayout:
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + mob_number));
+                startActivity(callIntent);
+                return;
+            case R.id.shopMsgLayout:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", mob_number, null)));
+                return;
             case R.id.shopEditLayout:
-                Toast.makeText(this, "Edit Post", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, AddPostActivity.class));
                 return;
             case R.id.shopShareLayout:
                 Toast.makeText(this, "Share Post", Toast.LENGTH_SHORT).show();
                 return;
-
-
-
         }
     }
+
 }
