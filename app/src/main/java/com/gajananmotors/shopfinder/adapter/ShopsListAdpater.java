d6088084a7ea3d06a69841f46ff939aae1da3359
dell <patilganesh6200@gmail.com>
2018-02-28 22:13:22 +0530

    all changes done

--- app/src/main/java/com/gajananmotors/shopfinder/adapter/ShopsListAdpater.java
+++ app/src/main/java/com/gajananmotors/shopfinder/adapter/ShopsListAdpater.java
@@ -118,7 +118,7 @@ public class ShopsListAdpater extends RecyclerView.Adapter<ShopsListHolder> impl
                     if (!isNetworkAvailable(activity)) {
                         displayPromptForEnablingData(activity);
                     } else {
-                        String shopName=holder.name.getText().toString();
+                        String shopName = holder.name.getText().toString();
                         String uriBegin = "geo:" + viewShopList.getLatitude() + "," + viewShopList.getLongitude();
                         String query = viewShopList.getLatitude() + "," + viewShopList.getLongitude() + "(" + name + ")";
                         String encodedQuery = Uri.encode(query);
@@ -126,9 +126,9 @@ public class ShopsListAdpater extends RecyclerView.Adapter<ShopsListHolder> impl
                         Uri uri = Uri.parse(uriString);
                         String address = getAddress(lat, lng, activity);
                         Log.d("MultiViewType", "address" + address);
-                      Uri gmmIntentUri = Uri.parse("geo:" + lat + "," + lng +"?q=" +shopName+"+" +address);
-                       // Uri gmmIntentUri = Uri.parse("geo:navigation" + 0 + "," + 0 + "?q=" + address);
-                        Log.d("uriIntent","gmmIntentUri"+gmmIntentUri);
+                        Uri gmmIntentUri = Uri.parse("geo:" + lat + "," + lng + "?q=" + shopName + "+" + address);
+                        // Uri gmmIntentUri = Uri.parse("geo:navigation" + 0 + "," + 0 + "?q=" + address);
+                        Log.d("uriIntent", "gmmIntentUri" + gmmIntentUri);
                         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
                         mapIntent.setPackage("com.google.android.apps.maps");
                         activity.startActivity(mapIntent);
@@ -172,44 +172,45 @@ public class ShopsListAdpater extends RecyclerView.Adapter<ShopsListHolder> impl
                 @Override
                 public void onClick(View v) {
 
-                viewShopList.setShop_id(list.get(position).getShop_id());
-                viewShopList.setStrShop_name(list.get(position).getShop_name());
-                viewShopList.setStrAddress(list.get(position).getAddress());
-                viewShopList.setLatitude(Double.parseDouble(list.get(position).getShop_lat()));
-                viewShopList.setLongitude(Double.parseDouble(list.get(position).getShop_long()));
-                viewShopList.setStrCategory(list.get(position).getCategory_name());
-                viewShopList.setStrSub_category(list.get(position).getSub_category_name());
-                viewShopList.setStrWeburl(list.get(position).getWebsite());
-                viewShopList.setStrMobile(list.get(position).getShop_mob_no());
-                viewShopList.setStrShopTime(list.get(position).getShop_timing());
-                viewShopList.setStrservices(list.get(position).getServices());
-                images.clear();
-                index = 0;
-                if (list.get(position).getImage1() != null) {
-                    images.add(index++, list.get(position).getImage1());
+                    viewShopList.setShop_id(list.get(position).getShop_id());
+                    viewShopList.setStrShop_name(list.get(position).getShop_name());
+                    viewShopList.setStrAddress(list.get(position).getAddress());
+                    viewShopList.setLatitude(Double.parseDouble(list.get(position).getShop_lat()));
+                    viewShopList.setLongitude(Double.parseDouble(list.get(position).getShop_long()));
+                    viewShopList.setStrCategory(list.get(position).getCategory_name());
+                    viewShopList.setStrSub_category(list.get(position).getSub_category_name());
+                    viewShopList.setStrWeburl(list.get(position).getWebsite());
+                    viewShopList.setStrMobile(list.get(position).getShop_mob_no());
+                    viewShopList.setStrShopTime(list.get(position).getShop_timing());
+                    viewShopList.setStrservices(list.get(position).getShop_details());
+                    images.clear();
+                    index = 0;
+                    if (list.get(position).getImage1() != null) {
+                        images.add(index++, list.get(position).getImage1());
+                    }
+                    if (list.get(position).getImage2() != null) {
+                        images.add(index++, list.get(position).getImage2());
+                    }
+                    if (list.get(position).getImage3() != null) {
+                        images.add(index++, list.get(position).getImage3());
+                    }
+                    if (list.get(position).getImage4() != null) {
+                        images.add(index++, list.get(position).getImage4());
+                    }
+                    if (list.get(position).getImage5() != null) {
+                        images.add(index++, list.get(position).getImage5());
+                    }
+                    if (list.get(position).getImage6() != null) {
+                        images.add(index++, list.get(position).getImage6());
+                    }
+                    viewShopList.setArrayList(images);
+                    viewShopList.setStrShop_pic(list.get(position).getShop_pic());
+                    transition(position);
+                    //activity.finish();
+                    Log.e("updatedsize", String.valueOf(images.size()));
                 }
-                if (list.get(position).getImage2() != null) {
-                    images.add(index++, list.get(position).getImage2());
-                }
-                if (list.get(position).getImage3() != null) {
-                    images.add(index++, list.get(position).getImage3());
-                }
-                if (list.get(position).getImage4() != null) {
-                    images.add(index++, list.get(position).getImage4());
-                }
-                if (list.get(position).getImage5() != null) {
-                    images.add(index++, list.get(position).getImage5());
-                }
-                if (list.get(position).getImage6() != null) {
-                    images.add(index++, list.get(position).getImage6());
-                }
-                viewShopList.setArrayList(images);
-                viewShopList.setStrShop_pic(list.get(position).getShop_pic());
-                transition(position);
-                //activity.finish();
-                Log.e("updatedsize", String.valueOf(images.size()));
-            }
-        });
+            });
+        }
     }
     private void transition(int position) {
         Log.d("Allpost", "transition");
